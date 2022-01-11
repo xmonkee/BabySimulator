5eb4b03f1b6665e0867ed01d0454ad4853315a52
Mayank Mandava <mayankmandava@gmail.com>
2022-01-11 01:10:45 +0000

    First commit

---
+++ baby.lua
@@ -0,0 +1,16 @@
+t=0
+x=96
+y=24
+
+function TIC()
+
+	if btn(0) then y=y-1 end
+	if btn(1) then y=y+1 end
+	if btn(2) then x=x-1 end
+	if btn(3) then x=x+1 end
+
+	cls(10)
+	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
+	print("HELLO WORLD!",84,84)
+	t=t+1
+end
